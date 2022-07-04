efb4ebc5ddd53f420f45b8101687d9e48179bde4
Elavarasu <elavarasu.subramaniam@gmail.com>
2022-07-04 14:33:06 +0530

    Initial-commit

---
+++ quiz.js
@@ -0,0 +1,27 @@
+var name = prompt("1.The Finest sprinter")
+if(name == "usain bolt")
+{
+  alert("Congrats")
+}
+else 
+{
+   name = prompt("He is a jamaican")
+   if(name == "usian bolt")
+   {
+    alert("congrats")
+   }
+   else
+   {
+    alert("Sorry try another clue")
+   }
+
+   name = prompt("He is world record holder")
+   if(name == "usain bolt")
+   {
+    alert("congrats")
+   }
+   else
+   {
+     alert("try again")
+   }
+}
