d39246ed9ac223cf40feacaacd92996b169ba8bf
Elavarasu13 <elavarasu.subramaniam@gmail.com>
2025-09-01 12:40:36 +0530

    Learning functions

---
+++ inbuilt.js
@@ -0,0 +1,9 @@
+/*learning ForEach*/
+
+let arr = [1,2,3]
+
+arr.forEach(n => console.log(n*2));
+
+let fruits = ['apple','banana','pomogranate'];
+
+fruits.forEach(fruit => console.log(fruit.toUpperCase()));
